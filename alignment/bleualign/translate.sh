2e756770000cf5a7d0010f3929f6dbec02fcde83
Boxiang Liu <jollier.liu@gmail.com>
2019-11-02 12:14:57 -0700

    modified:   alignment/bleualign/translate.sh

--- alignment/bleualign/translate.sh
+++ alignment/bleualign/translate.sh
@@ -1,10 +1,23 @@
-onmt=~/projects/OpenNMT-py/onmt/
-data=/mnt/data/boxiang/wmt18/
-model=~/projects/med_translation/processed_data/translation/wmt18/
+export onmt=~/projects/OpenNMT-py/onmt/
+export data=/mnt/data/boxiang/wmt18/
+export model=../processed_data/translation/wmt18/
+export src_dir=../processed_data/preprocess/sentences/
+export out_dir=../processed_data/alignment/bleualign/translate/
 
-$onmt/bin/translate.py \
--model $model/zh-en_2_step_120000.pt \
--src ../processed_data/preprocess/sentences/鼻咽癌的吉西他滨联合顺铂诱导化疗.zh.tok \
--output ../processed_data/alignment/bleualign/translate/鼻咽癌的吉西他滨联合顺铂诱导化疗.zh.to_en \
--replace_unk -verbose \
--gpu 0
+translate(){
+	src=$1
+	gpu=$(($2 % 10))
+
+	echo "Source file:" $src
+	echo "GPU:" $gpu
+
+	$onmt/bin/translate.py \
+	-model $model/zh-en_2_step_120000.pt \
+	-src $src \
+	-output $out_dir/`basename $src`.to_en \
+	-replace_unk \
+	-gpu $gpu	
+}
+
+export -f translate
+parallel --jobs 10 translate {1} {2} ::: `ls $src_dir/*.zh.tok` :::+ {1..2049}
