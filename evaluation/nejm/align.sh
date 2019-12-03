b1e98a95cae61c29015d9d571227079ad92eac4e
Boxiang Liu <boxiang@asimov-0.svail.baidu.com>
2019-12-03 15:00:37 -0800

    modified:   evaluation/nejm/align.sh 	modified:   evaluation/nejm/evaluate.sh

--- evaluation/nejm/align.sh
+++ evaluation/nejm/align.sh
@@ -5,7 +5,7 @@ out_dir=/mnt/scratch/boxiang/projects/med_translation/processed_data/evaluation/
 [[ -f $out_dir ]] && mkdir -p $out_dir
 
 # Bleualign
-mkdir -p $out_dir/bleualign/
+mkdir -p $out_dir/ba/
 for doc in `ls $data/doc*_zh.snt`; do
 	$bleualign --factored \
 	-s $doc \
@@ -14,13 +14,13 @@ for doc in `ls $data/doc*_zh.snt`; do
 	--printempty --verbosity 2 \
 	-o ${doc/_zh.snt/.ba}
 done 
-cat $data/doc*.ba-s > $out_dir/bleualign/align.ba-s
-cat $data/doc*.ba-t > $out_dir/bleualign/align.ba-t
+cat $data/doc*.ba-s > $out_dir/ba/align.ba-s
+cat $data/doc*.ba-t > $out_dir/ba/align.ba-t
 rm $data/doc*.ba-{s,t}
 
 
 # Bleualign (both directions):
-mkdir -p $out_dir/bleualign2/
+mkdir -p $out_dir/ba2/
 for doc in `ls $data/doc*_zh.snt`; do
 	$bleualign --factored \
 	-s $doc \
@@ -30,13 +30,13 @@ for doc in `ls $data/doc*_zh.snt`; do
 	--printempty --verbosity 2 \
 	-o ${doc/_zh.snt/.ba2}
 done 
-cat $data/doc*.ba2-s > $out_dir/bleualign/align.ba2-s
-cat $data/doc*.ba2-t > $out_dir/bleualign/align.ba2-t
+cat $data/doc*.ba2-s > $out_dir/ba2/align.ba2-s
+cat $data/doc*.ba2-t > $out_dir/ba2/align.ba2-t
 rm $data/doc*.ba2-{s,t}
 
 
 # Gale-Church:
-mkdir -p $out_dir/galechurch/
+mkdir -p $out_dir/gc/
 for doc in `ls $data/doc*_zh.snt`; do
 	$bleualign --factored \
 	-s $doc \
@@ -48,8 +48,8 @@ for doc in `ls $data/doc*_zh.snt`; do
 	-o ${doc/_zh.snt/.gc}
 done 
 
-cat $data/doc*.gc-s > $out_dir/galechurch/align.gc-s
-cat $data/doc*.gc-t > $out_dir/galechurch/align.gc-t
+cat $data/doc*.gc-s > $out_dir/gc/align.gc-s
+cat $data/doc*.gc-t > $out_dir/gc/align.gc-t
 rm $data/doc*.gc-{s,t}
 
 # Moore's algorithm (IBM 1):
--- evaluation/nejm/evaluate.sh
+++ evaluation/nejm/evaluate.sh
@@ -4,19 +4,20 @@
 # gc: Gale-Church
 # moore: Moore's IBM 1 model.
 
+mkdir -p ../processed_data/evaluation/nejm/evaluate/
 for algo in ba ba2 gc moore; do
 
 	# This will generate src <=> tgt alignment. 
 	python3 evaluation/wmt19_biomed/gen_align_file.py \
-	--src_fn ../processed_data/evaluation/nejm/translation/align.${algo}-s \
-	--tgt_fn ../processed_data/evaluation/nejm/translation/align.${algo}-t \
-	--out_fn ../processed_data/evaluation/nejm/translation/align_${algo}_zh_en.txt
+	--src_fn ../processed_data/evaluation/nejm/align/$algo/align.${algo}-s \
+	--tgt_fn ../processed_data/evaluation/nejm/align/$algo/align.${algo}-t \
+	--out_fn ../processed_data/evaluation/nejm/align/$algo/align_${algo}_zh_en.txt
 
 
 	# Evaluate algorithm:
 	python3 evaluation/wmt19_biomed/evaluate.py \
 	--align_fn ../processed_data/preprocess/alignment/align_validation_zh_en.txt \
-	--pred_fn ../processed_data/evaluation/nejm/translation/align_${algo}_zh_en.txt \
-	--out_fn ../processed_data/evaluation/nejm/translation/evaluate/${algo}.pr
+	--pred_fn ../processed_data/evaluation/nejm/align/$algo/align_${algo}_zh_en.txt \
+	--out_fn ../processed_data/evaluation/nejm/evaluate/${algo}.pr
 
 done
