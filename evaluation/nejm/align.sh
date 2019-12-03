040cf02cd2a99a1623719bd0b613bad84986c1e1
Boxiang Liu <boxiang@asimov-0.svail.baidu.com>
2019-12-03 11:50:33 -0800

    modified:   evaluation/nejm/align.sh 	modified:   evaluation/nejm/translate.sh

--- evaluation/nejm/align.sh
+++ evaluation/nejm/align.sh
@@ -1,8 +1,11 @@
 bleualign=~/projects/Bleualign/bleualign.py
 moore=~/software/bilingual-sentence-aligner/
 data=/mnt/scratch/boxiang/projects/med_translation/processed_data/evaluation/nejm/translation/
+out_dir=/mnt/scratch/boxiang/projects/med_translation/processed_data/evaluation/nejm/align/
+[[ -f $out_dir ]] && mkdir -p $out_dir
 
 # Bleualign
+mkdir -p $out_dir/bleualign/
 for doc in `ls $data/doc*_zh.snt`; do
 	$bleualign --factored \
 	-s $doc \
@@ -11,12 +14,13 @@ for doc in `ls $data/doc*_zh.snt`; do
 	--printempty --verbosity 2 \
 	-o ${doc/_zh.snt/.ba}
 done 
-cat $data/doc*.ba-s > $data/align.ba-s
-cat $data/doc*.ba-t > $data/align.ba-t
+cat $data/doc*.ba-s > $out_dir/bleualign/align.ba-s
+cat $data/doc*.ba-t > $out_dir/bleualign/align.ba-t
 rm $data/doc*.ba-{s,t}
 
 
 # Bleualign (both directions):
+mkdir -p $out_dir/bleualign2/
 for doc in `ls $data/doc*_zh.snt`; do
 	$bleualign --factored \
 	-s $doc \
@@ -26,12 +30,13 @@ for doc in `ls $data/doc*_zh.snt`; do
 	--printempty --verbosity 2 \
 	-o ${doc/_zh.snt/.ba2}
 done 
-cat $data/doc*.ba2-s > $data/align.ba2-s
-cat $data/doc*.ba2-t > $data/align.ba2-t
+cat $data/doc*.ba2-s > $out_dir/bleualign/align.ba2-s
+cat $data/doc*.ba2-t > $out_dir/bleualign/align.ba2-t
 rm $data/doc*.ba2-{s,t}
 
 
 # Gale-Church:
+mkdir -p $out_dir/galechurch/
 for doc in `ls $data/doc*_zh.snt`; do
 	$bleualign --factored \
 	-s $doc \
@@ -43,23 +48,27 @@ for doc in `ls $data/doc*_zh.snt`; do
 	-o ${doc/_zh.snt/.gc}
 done 
 
-cat $data/doc*.gc-s > $data/align.gc-s
-cat $data/doc*.gc-t > $data/align.gc-t
+cat $data/doc*.gc-s > $out_dir/galechurch/align.gc-s
+cat $data/doc*.gc-t > $out_dir/galechurch/align.gc-t
 rm $data/doc*.gc-{s,t}
 
 # Moore's algorithm (IBM 1):
 
 # Adding parallel corpora for IBM 1 model construction.
+mkdir -p $data/tmp/
 head -n 100000 /mnt/data/boxiang/wmt18/train/corpus.zh > \
-$data/wmt18_zh.snt
+$data/tmp/wmt18_zh.snt
 head -n 100000 /mnt/data/boxiang/wmt18/train/corpus.en > \
-$data/wmt18_en.snt
+$data/tmp/wmt18_en.snt
+ln $data/doc* $data/tmp/
 
 # Moore's algorithm is modified to allow factored sentences. 
 # One can factor sentences by the pipe "|" character. Only the 
 # first factor will be used in alignment
+mkdir -p $out_dir/moore/
 cd $moore # Must run in this directory
 perl $moore/align-sents-all-multi-file.pl \
-$data/ 0.5
-cat $data/doc*_zh.snt.aligned > $data/align.moore-s
-cat $data/doc*_en.snt.aligned > $data/align.moore-t
+$data/tmp 0.5
+cat $data/tmp/doc*_zh.snt.aligned > $out_dir/moore/align.moore-s
+cat $data/tmp/doc*_en.snt.aligned > $out_dir/moore/align.moore-t
+# rm $data/tmp/
--- evaluation/nejm/translate.sh
+++ evaluation/nejm/translate.sh
@@ -8,7 +8,16 @@ export out_dir=../processed_data/evaluation/nejm/translation/
 # Articles:
 articles=(鼻咽癌的吉西他滨联合顺铂诱导化疗 \
 	饮水可对饮用含糖饮料产生多大程度的对抗作用 \
-	帕妥珠单抗和曲妥珠单抗辅助治疗早期HER2阳性乳腺癌)
+	帕妥珠单抗和曲妥珠单抗辅助治疗早期HER2阳性乳腺癌 \
+	转移性去势抵抗性前列腺癌的恩杂鲁胺耐药 \
+	婴儿B群链球菌疾病预防指南更新 \
+	黑种人理发店可帮助顾客降血压 \
+	内科患者应用阿哌沙班和依诺肝素预防血栓形成的比较 \
+	尼拉帕尼用于铂类敏感型复发性卵巢癌的维持治疗 \
+	膀胱切除术的最佳手术方法：开放式手术与机器人辅助手术的比较 \
+	1型糖尿病患者胰岛素治疗中加用sotagliflozin的效果 \
+	HIV相关癌症和疾病 \
+	2017年慢性阻塞性肺疾病诊断和治疗的GOLD指南)
 
 # Iterate through all articles:
 n=0
@@ -30,6 +39,7 @@ for article in ${articles[@]}; do
 	-src $out_dir/${article}.zh.tok.bpe \
 	-output $out_dir/${article}.zh.tok.bpe.2en \
 	-replace_unk -verbose \
+	-batch_size 1 \
 	-gpu 0
 
 	# Remove BPE characters:
@@ -50,6 +60,7 @@ for article in ${articles[@]}; do
 	-src $out_dir/${article}.en.tok.bpe \
 	-output $out_dir/${article}.en.tok.bpe.2zh \
 	-replace_unk -verbose \
+	-batch_size 1 \
 	-gpu 0
 
 	# Remove BPE characters:
