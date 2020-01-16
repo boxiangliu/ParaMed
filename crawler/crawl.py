3a3604a566efa623ca31a633f1fdf1548621da9b
Boxiang Liu <jollier.liu@gmail.com>
2020-01-16 10:23:52 -0800

    modified crawler/crawl.py In crawler/article_stat.py, I explored the difference between *.filt.* and *.full.*. Some Chinese and English filt files differ greatly in length because some sentences are missing. This is because the full_text and partial text don't match exactly, and therefore later during the matching stage, some text from the full_text will be filtered out (see crawl/crawler.py) for details of the matching process.
    An alternative would be to use the full_text as a start, and then remove the following things:
    1. Figure and table captions
    2. Anything after disclosure
    3. Anything before "comment opens"
    4. Miscellaneous things

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -29,57 +29,89 @@ os.makedirs(out_dir, exist_ok=True)
 os.makedirs(article_dir, exist_ok=True)
 
 
-# Functions:
-def to_include(element):
-	include = True
-	text = element.text
+#####################
+# Utility Functions #
+#####################
+def print_and_log(message):
+	print(message, flush=True)
+	logging.info(message)
 
 
-	table = re.match("表[0-9]{1,2}\.", text)
-	figure = re.match("图[0-9]{1,2}\.", text)
-	if text == "":
-		include = False
+def detect_dialog_window(driver):
 
-	if table or figure:
-		include = False
-
-	if element.get_attribute("label") == "图片说明":
-		include = False
-
-	if "译者：" in text:
-		include = False
-
-	if "校对：" in text:
-		include = False
-
-	return include
+	window = driver.find_elements_by_xpath(\
+		"//div[@class='featherlight-content']")
+	if window != []:
+		return True
+	else:
+		return False
 
 
-def stop_crawling(element):
-	stop_crawl = False
+def close_dialog_window(driver):
 
-	if element.text.strip().startswith("Disclosure forms provided"):
-		stop_crawl = True
-
-	if element.text.strip().startswith("Disclosures for") \
-		and element.text.strip().endswith("at time of publication"):
-		stop_crawl = True
-
-	return stop_crawl
+	close_button = driver.find_element_by_xpath(
+		"//button[@class='featherlight-close-icon featherlight-close']")
+	close_button.click()
 
 
-def start_crawling(element):
-	crawl = False
-	if element.get_attribute("class") == \
-		"f-body f-body--w-dropcap":
-		crawl = True
-	if element.get_attribute("class") == \
-		"o-article-body__section-title a-article-h1 a-article-h1--underline f-h3" \
-		and element.text == "Abstract":
-		crawl = True
-	return crawl
+def detect_paywall(driver):
+	xpath_query = "//a[@class='o-gateway__button o-gateway__button--secondary'" \
+				  " and @data-interactiontype='subscribe_click']"
+
+	paywall = driver.find_elements_by_xpath(xpath_query)
+	if paywall != []:
+		return True
+	else:
+		return False
 
 
+def yxqy_login(driver):
+
+	try:
+		xpath_query = "//a[@href='javascript:;' and @class='dropdown-toggle']"
+		dropdown = driver.find_element_by_xpath(xpath_query)
+		dropdown.click()
+		membername = driver.find_element_by_name("membername")
+		membername.clear()
+		membername.send_keys("publicuser")
+
+		password = driver.find_element_by_name("password")
+		password.clear()
+		password.send_keys("publicuser")
+
+		login = driver.find_element_by_class_name(\
+			"btn.btn-default.fastLoginBtn.login-top")
+		login.click()
+		sleep(2)
+
+	except:
+		print("Already logged in to YXQY!")
+		logged_in = True
+
+
+def jw_login(driver):
+	try:
+		email = driver.find_element_by_xpath(\
+			"//article-page//input[@id='email_text']")
+		email.clear()
+		email.send_keys("bliuforgit@gmail.com")
+		password = driver.find_element_by_xpath(\
+			"//article-page//input[@id='pwd_text']")
+		password.clear()
+		password.send_keys("password")
+		login = driver.find_element_by_xpath(\
+			"//article-page//button")
+		login.click()
+		sleep(3)
+
+	except:
+		print("Already logged into Journal Watch!")
+		logged_in = True
+
+
+######################
+# Crawling Functions #
+######################
 def zh_unwanted(x):
 	if re.match("表[0-9]{1,2}\.", x) or \
 		re.match("图[0-9]{1,2}\.", x) or \
@@ -119,78 +151,6 @@ def crawl_zh_page(driver, article_id, zh_url, out_prefix, verbose=False):
 			f.write(i + "\n")
 
 
-def yxqy_login(driver):
-
-	try:
-		xpath_query = "//a[@href='javascript:;' and @class='dropdown-toggle']"
-		dropdown = driver.find_element_by_xpath(xpath_query)
-		dropdown.click()
-		membername = driver.find_element_by_name("membername")
-		membername.clear()
-		membername.send_keys("publicuser")
-
-		password = driver.find_element_by_name("password")
-		password.clear()
-		password.send_keys("publicuser")
-
-		login = driver.find_element_by_class_name(\
-			"btn.btn-default.fastLoginBtn.login-top")
-		login.click()
-		sleep(2)
-
-	except:
-		print("Already logged in to YXQY!")
-		logged_in = True
-
-
-def jw_login(driver):
-	try:
-		email = driver.find_element_by_xpath("//article-page//input[@id='email_text']")
-		email.clear()
-		email.send_keys("bliuforgit@gmail.com")
-		password = driver.find_element_by_xpath("//article-page//input[@id='pwd_text']")
-		password.clear()
-		password.send_keys("password")
-		login = driver.find_element_by_xpath("//article-page//button")
-		login.click()
-		sleep(3)
-
-	except:
-		print("Already logged into Journal Watch!")
-		logged_in = True
-
-def print_and_log(message):
-	print(message, flush=True)
-	logging.info(message)
-
-
-def detect_dialog_window(driver):
-
-	window = driver.find_elements_by_xpath("//div[@class='featherlight-content']")
-	if window != []:
-		return True
-	else:
-		return False
-
-
-def close_dialog_window(driver):
-
-	close_button = driver.find_element_by_xpath(
-		"//button[@class='featherlight-close-icon featherlight-close']")
-	close_button.click()
-
-
-def detect_paywall(driver):
-	xpath_query = "//a[@class='o-gateway__button o-gateway__button--secondary'" \
-				  " and @data-interactiontype='subscribe_click']"
-
-	paywall = driver.find_elements_by_xpath(xpath_query)
-	if paywall != []:
-		return True
-	else:
-		return False
-
-
 def en_unwanted(x):
 	if x.text == "" or \
 		x.get_attribute("class") == "m-media-item" or \
@@ -214,6 +174,7 @@ def remove_text_after_disclosure(l):
 
 def crawl_en_page(driver, article_id, en_url, out_prefix, verbose=False):
 	driver.get(en_url)
+	
 	if detect_dialog_window(driver):
 		close_dialog_window(driver)
 
@@ -221,9 +182,10 @@ def crawl_en_page(driver, article_id, en_url, out_prefix, verbose=False):
 		raise RuntimeError("Paywall detected.")
 
 	print_and_log(f"Crawling English article: {article_id}.")
-	article_type = re.sub("[0-9]+", "", article_id)
+	article_type = re.sub("[0-9]+", "", article_id).replace("%", "")
 	print_and_log(f"Article type: {article_type}.")
 
+	# Crawl article from NEJM website
 	if article_type != "jw.na":
 		full_article = driver.find_element_by_id("full")
 		full_text = [x.strip() for x in full_article.text.split("\n")]
@@ -239,9 +201,36 @@ def crawl_en_page(driver, article_id, en_url, out_prefix, verbose=False):
 		paragraph_h2_h3_set = set(paragraph_filtered + h2_filtered + h3_filtered)
 		filtered_text = [x for x in full_text if x in paragraph_h2_h3_set]
 		filtered_text = remove_text_after_disclosure(filtered_text)
+
+	# Crawl article from Journal Watch website
 	else:
+		try:
+			WebDriverWait(driver, timeout=60).\
+				until(EC.presence_of_element_located(\
+					(By.CLASS_NAME, "article-detail")))
+			sleep(1)
+		except:
+			print("Timeout!")
+			return
+
 		jw_login(driver)
-		pass # Put Journal Watch code here:
+		full_article = driver.find_element_by_class_name("article-detail")
+		full_text = [x.strip() for x in full_article.text.split("\n")]
+
+		title = driver.find_element_by_class_name("article-detail__header").\
+			find_element_by_tag_name("h1").text.split("\n")
+		content = driver.find_element_by_class_name(\
+			"article-detail__content").text.split("\n")[1:] # Remove author line.
+		try:
+			comment = driver.find_element_by_class_name(\
+				"article-detail__comment").text.split("\n")
+		except:
+			comment = []
+		disclosure = driver.find_element_by_class_name(\
+			"article-detail__disclosures").text.split("\n")
+
+		tccd_set = set(title + content + comment + disclosure)
+		filtered_text = [x for x in full_text if x in tccd_set]
 
 	with open(f"{out_prefix}.full.en", "w") as f:
 		for i in full_text:
@@ -250,43 +239,6 @@ def crawl_en_page(driver, article_id, en_url, out_prefix, verbose=False):
 	with open(f"{out_prefix}.filt.en", "w") as f:
 		for i in filtered_text:
 			f.write(i + "\n")
-
-en_url = "http://www.nejm.org/doi/full/10.1056/NEJMoa1602074"
-article_id = "oa1602074"
-out_prefix = "test"
-crawl_en_page(driver, article_id, en_url, out_prefix)
-
-
-def crawl_en_journal_watch(driver, output, timeout=60, verbose=False):
-
-	print_and_log("Crawling English Journal Watch.")
-
-	try: 
-		WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Comment') or contains(text(), 'Disclosures for')]")))
-	except:
-		print("Timeout.")
-		return
-
-	with open(output, "w") as f:
-
-		sleep(4)
-		
-		xpath_query = "//*[self::h3 or self::p]"
-		paragraphs = driver.find_elements_by_xpath(xpath_query)
-
-		for i, p in enumerate(paragraphs):
-
-			if verbose: 
-				print("Parsing paragraph {}".format(i), flush=True)
-
-			if stop_crawling(p):
-				print("Stopped crawling at paragraph {}.".format(i))
-				return
-
-			if to_include(p):
-				f.write(p.text)
-
-			f.write("\n")
 
 
 def compare_zh_and_en(zh_fn, en_fn, epsilon = 2):
@@ -304,16 +256,16 @@ def compare_zh_and_en(zh_fn, en_fn, epsilon = 2):
 
 	else:
 		if en_len / zh_len > epsilon:
-			comparison = "en_long"
+			comparison = "en_too_long"
 		elif zh_len / en_len > epsilon:
-			comparison = "zh_long"
+			comparison = "zh_too_long"
 		else:
 			comparison = "equal"
 
 	return comparison, zh_len, en_len 
 
 
-def crawl_all_urls(container):
+def crawl_all_urls(driver, container):
 
 	total = len([_ for i in container.values() \
 		for j in i.values() for k in j.values()])
@@ -328,9 +280,9 @@ def crawl_all_urls(container):
 		for month, article_dict in month_dict.items():
 			os.makedirs(os.path.join(article_dir, year, month), exist_ok=True)
 
-			print_and_log("######################")
+			print_and_log("####################")
 			print_and_log(f"# Crawling {year}/{month} #")
-			print_and_log("######################")
+			print_and_log("####################")
 
 			for article_id, (zh_title, en_title, zh_url, en_url) in article_dict.items():
 
@@ -342,8 +294,8 @@ def crawl_all_urls(container):
 				print_and_log(message)
 
 				out_prefix = f"{article_dir}/{year}/{month}/{article_id}"
-				zh_out = out_prefix + ".zh"
-				en_out = out_prefix + ".en"
+				zh_out = f"{out_prefix}.filt.zh"
+				en_out = f"{out_prefix}.filt.en"
 
 				to_run = True
 				if os.path.exists(zh_out) and os.path.exists(en_out):
@@ -361,19 +313,10 @@ def crawl_all_urls(container):
 						print_and_log("zh and en articles differ - rerunning.")
 
 				if to_run:
-					
-					# Crawl Chinese article:
+					# Crawl articles:
 					crawl_zh_page(driver, article_id, zh_url, out_prefix)
+					crawl_en_page(driver, article_id, en_url, out_prefix)
 
-					# Crawl English article:
-					driver.get(en_url)
-					_type = "journal_watch" if "jw" in article_id else "nejm"
-					if _type == "nejm":
-						crawl_en_nejm(driver, en_out)
-
-					elif _type == "journal_watch":
-						crawl_en_journal_watch(driver, en_out)
-					
 				n += 1
 
 
@@ -401,8 +344,9 @@ def main():
 	# Logging:
 	if crawl:
 		log_fn = "{}/article.log".format(article_dir)
-		logging.basicConfig(filename=log_fn, format="%(message)s")
-		crawl_all_urls(container)
+		logging.basicConfig(filename=log_fn, \
+			format="%(message)s", level=logging.DEBUG)
+		crawl_all_urls(driver, container)
 
 
 if __name__ == "__main__":
