9abd6c45b7f94f86c32f108ffe1716825fa8d319
Boxiang Liu <jollier.liu@gmail.com>
2021-05-09 13:09:59 -0700

    modified:   translation/wmt18/train_rnn.sh

--- translation/wmt18/train_rnn.sh
+++ translation/wmt18/train_rnn.sh
@@ -20,37 +20,37 @@ echo "Output dir = $OUT"
 [ -d $OUT/test ] || mkdir -p  $OUT/test/{zh2en,en2zh}
 
 
-echo "Step 1a: Preprocess inputs"
-echo "BPE on source"
-$ONMT/tools/learn_bpe.py -v -s $BPE_OPS < $TRAIN.$SRC > $OUT/data/bpe-codes.$SRC
-$ONMT/tools/learn_bpe.py -v -s $BPE_OPS < $TRAIN.$TGT > $OUT/data/bpe-codes.$TGT
+# echo "Step 1a: Preprocess inputs"
+# echo "BPE on source"
+# $ONMT/tools/learn_bpe.py -v -s $BPE_OPS < $TRAIN.$SRC > $OUT/data/bpe-codes.$SRC
+# $ONMT/tools/learn_bpe.py -v -s $BPE_OPS < $TRAIN.$TGT > $OUT/data/bpe-codes.$TGT
 
-$ONMT/tools/apply_bpe.py -c $OUT/data/bpe-codes.$SRC < $TRAIN.$SRC > $OUT/data/train.src
-$ONMT/tools/apply_bpe.py -c $OUT/data/bpe-codes.$SRC < $VALID.$SRC > $OUT/data/valid.src
-$ONMT/tools/apply_bpe.py -c $OUT/data/bpe-codes.$TGT < $TRAIN.$TGT > $OUT/data/train.tgt
-$ONMT/tools/apply_bpe.py -c $OUT/data/bpe-codes.$TGT < $VALID.$TGT > $OUT/data/valid.tgt
+# $ONMT/tools/apply_bpe.py -c $OUT/data/bpe-codes.$SRC < $TRAIN.$SRC > $OUT/data/train.src
+# $ONMT/tools/apply_bpe.py -c $OUT/data/bpe-codes.$SRC < $VALID.$SRC > $OUT/data/valid.src
+# $ONMT/tools/apply_bpe.py -c $OUT/data/bpe-codes.$TGT < $TRAIN.$TGT > $OUT/data/train.tgt
+# $ONMT/tools/apply_bpe.py -c $OUT/data/bpe-codes.$TGT < $VALID.$TGT > $OUT/data/valid.tgt
 
 #: <<EOF
-echo "Step 1b: Preprocess"
+# echo "Step 1b: Preprocess"
 # zh -> en
-python $ONMT/preprocess.py \
-    -src_seq_length 999 \
-    -tgt_seq_length 999 \
-    -train_src $OUT/data/train.src \
-    -train_tgt $OUT/data/train.tgt \
-    -valid_src $OUT/data/valid.src \
-    -valid_tgt $OUT/data/valid.tgt \
-    -save_data $OUT/data/zh2en/processed
+# python $ONMT/preprocess.py \
+#     -src_seq_length 999 \
+#     -tgt_seq_length 999 \
+#     -train_src $OUT/data/train.src \
+#     -train_tgt $OUT/data/train.tgt \
+#     -valid_src $OUT/data/valid.src \
+#     -valid_tgt $OUT/data/valid.tgt \
+#     -save_data $OUT/data/zh2en/processed
 
-# en -> zh
-python $ONMT/preprocess.py \
-    -src_seq_length 999 \
-    -tgt_seq_length 999 \
-    -train_src $OUT/data/train.tgt \
-    -train_tgt $OUT/data/train.src \
-    -valid_src $OUT/data/valid.tgt \
-    -valid_tgt $OUT/data/valid.src \
-    -save_data $OUT/data/en2zh/processed
+# # en -> zh
+# python $ONMT/preprocess.py \
+#     -src_seq_length 999 \
+#     -tgt_seq_length 999 \
+#     -train_src $OUT/data/train.tgt \
+#     -train_tgt $OUT/data/train.src \
+#     -valid_src $OUT/data/valid.tgt \
+#     -valid_tgt $OUT/data/valid.src \
+#     -save_data $OUT/data/en2zh/processed
 
 
 echo "Step 2: Train"
@@ -72,9 +72,8 @@ python restartsub.py TitanXx8 8 zh2en \
 -label_smoothing 0.1 \
 -valid_steps 10000 \
 -save_checkpoint_steps 5000 \
--world_size 8 \
--gpu_ranks 0 1 2 3 4 5 6 7 \
--master_port 10003"
+-world_size 1 \
+-gpu_ranks 0"
 
 
 python restartsub.py TitanXx8 8 en2zh \
@@ -94,6 +93,5 @@ python restartsub.py TitanXx8 8 en2zh \
 -label_smoothing 0.1 \
 -valid_steps 10000 \
 -save_checkpoint_steps 5000 \
--world_size 8 \
--gpu_ranks 0 1 2 3 4 5 6 7 \
--master_port 10003"
+-world_size 1 \
+-gpu_ranks 0"
